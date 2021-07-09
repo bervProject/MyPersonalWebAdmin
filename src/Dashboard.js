666291f442e03f10b0742a5e0d2105db5780907f
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-07-09 20:48:13 +0700

    Fix props

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,38 +3,55 @@ import { Card, CardHeader, CardContent, Grid } from "@material-ui/core";
 import { Title } from "react-admin";
 import feathersClient from './feathersClient';
 export default () => {
-  const [totalAdmin, setTotalAdmin] = useState(0);
-  const [totalUser, setTotalUser] = useState(0);
-  const [totalExp, setTotalExp] = useState(0);
-  const [totalEdu, setTotalEdu] = useState(0);
-  const [totalPorto, setTotalPorto] = useState(0);
-  const [totalPost, setTotalPost] = useState(0);
+  const [count, setCount] = useState({
+    totalAdmin: 0,
+    totalUser: 0,
+    totalExp: 0,
+    totalEdu: 0,
+    totalPorto: 0,
+    totalPost: 0
+  });
+
   useEffect(() => {
-    const adminTask = feathersClient.service("users").find({
+    feathersClient.service("users").find({
       query: {
         role: 'admin',
         $select: ['email']
       }
+    }).then(result => {
+      setCount((count) => ({ ...count, totalAdmin: result.total }));
     });
-    const userTask = feathersClient.service("users").find({
+  }, [count.totalAdmin]);
+
+  useEffect(() => {
+    feathersClient.service("users").find({
       query: {
-        role: {$ne:'admin'},
+        role: { $ne: 'admin' },
         $select: ['email']
       }
+    }).then(result => {
+      setCount((count) => ({ ...count, totalUser: result.total }));
     });
-    const experienceTask = feathersClient.service("experience").find();
-    const task = [adminTask, userTask, experienceTask];
-    Promise.all(task).then(results => {
-      const userAdminTotal = results[0].total;
-      const userTotal = results[1].total;
-      const expTotal = results[2].total;
-      setTotalAdmin(userAdminTotal);
-      setTotalUser(userTotal);
-      setTotalExp(expTotal);
-    }).catch(error => {
-      console.error(error);
+  }, [count.totalUser]);
+
+  useEffect(() => {
+    feathersClient.service("experience").find().then(result => {
+      setCount((count) => ({ ...count, totalExp: result.total }));
     });
-  });
+  }, [count.totalExp]);
+
+  useEffect(() => {
+    feathersClient.service("education").find().then(result => {
+      setCount((count) => ({ ...count, totalEdu: result.total }));
+    });
+  }, [count.totalEdu]);
+
+  useEffect(() => {
+    feathersClient.service("portofolio").find().then(result => {
+      setCount((count) => ({ ...count, totalPorto: result.total }));
+    });
+  }, [count.totalPorto]);
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={6}>
@@ -42,11 +59,11 @@ export default () => {
           <Title title="Welcome to the administration" />
           <CardHeader title="Users Data" />
           <CardContent>
-            <p>Total Admin: {totalAdmin}</p>
-            <p>Total Normal User: {totalUser}</p>
-            <p>Total Experiences Data: {totalExp}</p>
-            <p>Total Education Data: {totalEdu}</p>
-            <p>Total Portofolio Data: {totalPorto}</p>
+            <p>Total Admin: {count.totalAdmin}</p>
+            <p>Total Normal User: {count.totalUser}</p>
+            <p>Total Experiences Data: {count.totalExp}</p>
+            <p>Total Education Data: {count.totalEdu}</p>
+            <p>Total Portofolio Data: {count.totalPorto}</p>
           </CardContent>
         </Card>
       </Grid>
@@ -55,11 +72,10 @@ export default () => {
           <Title title="Welcome to the administration" />
           <CardHeader title="Blog Data" />
           <CardContent>
-            <p>Total Post: {totalPost}</p>
+            <p>Total Post: {count.totalPost}</p>
           </CardContent>
         </Card>
       </Grid>
     </Grid>
-
   )
 };
